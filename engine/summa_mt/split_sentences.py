fd8f350bad723eca9e4aefc7185cf1fb583e08af
Ulrich Germann <ulrich.germann@gmail.com>
2018-09-28 16:13:33 +0100

    Added NLTK sentence splitter for pt. Allow max. sentence length enforcement.

--- engine/summa_mt/split_sentences.py
+++ engine/summa_mt/split_sentences.py
@@ -6,15 +6,41 @@ Sentence splitting module. Wraps around ESERIX for some languages,
 TO DO: Eserix rules are actually just regular expressions. Consider implementing the sentence splitting directly in python
 """
 
-import sys,os
+import sys, os, logging, regex
 from subprocess import Popen, PIPE
 
 global ESERIX_CMD, ESERIX_RULES
-ESERIX_ROOT  = os.environ.get('ESERIX_ROOT','/opt/eserix')
-ESERIX_RULES = os.environ.get('ESERIX_RULES','%s/srx/rules.srx'%ESERIX_ROOT)
-ESERIX_CMD   = os.environ.get('ESERIX_CMD','%s/bin/eserix'%ESERIX_ROOT)
+
+logger = logging.getLogger(__name__)
+basedir  = os.path.dirname(__file__)
+
+ESERIX_RULES = None
+for guess in [os.environ.get('ESERIX_RULES', "%s/srx/rules.srx"%basedir)]:
+    if guess and os.path.exists(guess):
+        ESERIX_RULES = guess
+        break
+    else:
+        print("No luck:",guess)
+    pass
+if not ESERIX_RULES:
+    raise Exception("Cannot find Eserix rules!")
+
+ESERIX_CMD = None
+for guess in [os.environ.get('ESERIX_CMD', '%s/bin/eserix'%basedir)]:
+    if guess and os.path.exists(guess):
+        ESERIX_CMD = guess
+        break
+    pass
+if not ESERIX_CMD:
+    raise Exception("Cannot find Eserix executable.")
+
+# languages that Eserix can deal with
 eserix_languages = ["ar", "de", "en", "es", "fr", "hr", "pl", "ru", "zh"]
 
+# languages supported by nltk
+nltk_languages = ["pt"]
+
+# Other 
 supported_languages = eserix_languages + ["fa"] 
 
 class Eserix:
@@ -24,10 +50,21 @@ class Eserix:
         return
 
     def __call__(self,line):
-        p = Popen(self.cmd,stdin=PIPE,stdout=PIPE,bufsize=1)
+        p = Popen(self.cmd, stdin=PIPE, stdout=PIPE, bufsize=1)
         out,err = p.communicate(line.encode('utf8'))
         return out.decode('utf8').strip().split('\n')
+    pass
 
+class NLTK_SentenceSplitter:
+    def __init__(self, lang):
+        D = { "pt" : "portuguese" }
+        L = D[lang]
+        self.ssplit = nltk.data.load('tokenizers/punkt/%s.pickle'%L)
+        pass
+    def __call__(self,line):
+        return [regex.sub('\s+',' ',s).strip()
+                for x in self.ssplit.tokenize(line)]
+        
     pass
 
 class SimpleSentenceSplitter:
@@ -51,12 +88,16 @@ class SentenceSplitter:
 
     def __init__(self,lang,maxlen=0):
         if lang in eserix_languages: self.ssplit = Eserix(lang)
+        elif lang in nltk_languages: self.ssplit = NLTK_SentenceSplitter(lang)
         else: self.ssplit = SimpleSentenceSplitter(lang)
+        self.maxlen = maxlen
         return
 
-    
-    def __call__(self,text):
-        return self.ssplit(text)
+    def __call__(self, text):
+        sents = self.ssplit(text)
+        if self.maxlen:
+            sents = force_split_long_sentences(sents, self.maxlen)
+        return  sents
             
 if __name__ == "__main__":
     from argparse import ArgumentParser
@@ -100,14 +141,16 @@ if __name__ == "__main__":
                 buffer.append(line)
             elif len(buffer):
                 sents = ssplit(' '.join(buffer))
-                if opts.maxlen: sents = force_split_long_sentences(sents,opts.maxlen)
+                if opts.maxlen:
+                    sents = force_split_long_sentences(sents,opts.maxlen)
                 print('\n'.join(sents), end='\n\n',flush=opts.flush)
                 buffer = []
                 pass
             pass
         if len(buffer):
                 sents = ssplit(' '.join(buffer))
-                if opts.maxlen: sents = force_split_long_sentences(sents,opts.maxlen)
+                if opts.maxlen:
+                    sents = force_split_long_sentences(sents,opts.maxlen)
                 print('\n'.join(sents), end='\n\n',flush=opts.flush)
                 pass
         pass
